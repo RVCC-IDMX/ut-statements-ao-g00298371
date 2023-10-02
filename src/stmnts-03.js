69140c7e9709d5ede1eea292879eddbbbaa4d310
Your Name <you@example.com>
2023-10-01 20:32:05 -0400

    Unit Test 3 completed.

--- src/stmnts-03.js
+++ src/stmnts-03.js
@@ -14,7 +14,8 @@
  */
 
 function rectanglePerimeter(width, height) {
-  // write your code here & return
+  const perimeter = 2 * (width + height);
+  return parseFloat(perimeter.toFixed(2));
 }
 
 /**
@@ -25,7 +26,8 @@ function rectanglePerimeter(width, height) {
  *
  */
 function rectangleArea(width, height) {
-  // write your code here & return
+  const area = width * height;
+  return parseFloat(area.toFixed(2));
 }
 
 /**
@@ -39,7 +41,9 @@ function rectangleArea(width, height) {
  * Hint: use toFixed()
  */
 function circleCircumference(radius) {
-  // write your code here & return
+  const pi = Math.PI;
+  const circumference = 2 * pi * radius;
+  return parseFloat(circumference.toFixed(2));
 }
 
 /**
@@ -49,7 +53,9 @@ function circleCircumference(radius) {
  * @returns {number} - the area of the circle rounded to 2 decimal places
  */
 function circleArea(radius) {
-  // write your code here & return
+  const pi = Math.PI;
+  const area = pi * radius ** 2;
+  return parseFloat(area.toFixed(2));
 }
 
 /**
@@ -61,7 +67,8 @@ function circleArea(radius) {
  * @returns {number} - the area of the triangle rounded to 2 decimal places
  */
 function triangleArea(base, height) {
-  // write your code here & return
+  const area = 0.5 * base * height;
+  return parseFloat(area.toFixed(2));
 }
 
 /**
@@ -72,7 +79,8 @@ function triangleArea(base, height) {
  * @returns {number} - the hypotenuse of the triangle rounded to 2 decimal places
  */
 function pythagorean(sideA, sideB) {
-  // write your code here & return
+  const hypotenuse = Math.sqrt(sideA ** 2 + sideB ** 2);
+  return parseFloat(hypotenuse.toFixed(2));
 }
 
 module.exports = {
